ad421d19d964dfc335a096b8985f18fe1d8ee337
Jeremy Landis <jeremylandis@hotmail.com>
2016-08-13 11:46:36 -0400

    [config] Add new property set to false for when to search in parents for configuration

--- src/main/java/net/revelc/code/formatter/FormatterMojo.java
+++ src/main/java/net/revelc/code/formatter/FormatterMojo.java
@@ -365,6 +365,15 @@ public class FormatterMojo extends AbstractMojo implements ConfigurationSource {
     @Parameter(defaultValue = "false", property = "formatter.includeResources")
     private boolean includeResources;
 
+    /**
+     * Add configuration search for all parents of module executing formatting. The first found configuration including
+     * in module will be used.
+     *
+     * @since 2.18.0
+     */
+    @Parameter(defaultValue = "false", property = "addConfigSearchInParents")
+    private Boolean addConfigSearchInParents;
+
     /** The java formatter. */
     private final JavaFormatter javaFormatter = new JavaFormatter();
 
@@ -1001,15 +1010,17 @@ public class FormatterMojo extends AbstractMojo implements ConfigurationSource {
         this.getLog().debug("Using search path at: " + this.basedir.getAbsolutePath());
         this.resourceManager.addSearchPath(FileResourceLoader.ID, this.basedir.getAbsolutePath());
 
-        // Add parents
-        File parent = new File(Paths.get(this.basedir.toURI()).getParent() + File.separator + "pom.xml");
-        this.getLog().debug("Probing pom at: " + parent.getAbsolutePath());
-        while (parent.exists()) {
-            this.getLog().debug("Using search path at: " + parent.getParent());
-            this.resourceManager.addSearchPath(FileResourceLoader.ID, parent.getParent());
-
-            parent = new File(Paths.get(parent.toURI()).getParent().getParent() + File.separator + "pom.xml");
+        // Add parents for searching
+        if (this.addConfigSearchInParents) {
+            File parent = new File(Paths.get(this.basedir.toURI()).getParent() + File.separator + "pom.xml");
             this.getLog().debug("Probing pom at: " + parent.getAbsolutePath());
+            while (parent.exists()) {
+                this.getLog().debug("Using search path at: " + parent.getParent());
+                this.resourceManager.addSearchPath(FileResourceLoader.ID, parent.getParent());
+
+                parent = new File(Paths.get(parent.toURI()).getParent().getParent() + File.separator + "pom.xml");
+                this.getLog().debug("Probing pom at: " + parent.getAbsolutePath());
+            }
         }
 
         try (var configInput = this.resourceManager.getResourceAsInputStream(newConfigFile)) {
